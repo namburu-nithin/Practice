6a6db176329e6ac9cda0c06fab1386fa77221d01
namburu-nithin <74202573+namburu-nithin@users.noreply.github.com>
2021-12-18 23:35:48 +0530

    Update P3.py

--- P3.py
+++ P3.py
@@ -1,17 +1,19 @@
 '''a python function  that takes a sequence of integer  values and determines if there is a distinct pair of numbers in the sequence whose product is odd'''
-def odd_no(seq,N):
+def odd_no(seq, N):
   b=[]
   c=0
-  for i in range(N):
-    b.append(seq.pop(i))
-    if b[len(b)-1] not seq and b[len(b)-1]%2==1:
-      c+=1
-  if c>2:
-    return "yes'
+  for i in seq:
+    if i not in b:
+      b.append(i)
+    for i in b:
+      if i%2==1:
+        c+=1
+  if c>1:
+    return "yes"
   else:
     return "no"
 N=int(input())
 seq=[]
 for i in range(N):
   seq.append(int(input()))
-print(odd_no(seq,N))
+print(odd_no(seq, N))
